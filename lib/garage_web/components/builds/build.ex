ada5cae1a9fbe6dbcc1628dd4ba9cb01c4a8d0ef
Andy LeClair <andyleclair@gmail.com>
2024-04-16 11:41:26 -0400

    better menu

--- lib/garage_web/components/builds/build.ex
+++ lib/garage_web/components/builds/build.ex
@@ -20,19 +20,60 @@ defmodule GarageWeb.Components.Builds.Build do
         <% end %>
 
         <div class="p-6">
-          <h5 class="truncate mb-2 text-4xl font-medium leading-tight text-neutral-800">
-            <.link navigate={~p"/builds/#{@build.slug}"}>
-              <%= @build.name %>
-            </.link>
-          </h5>
-          <h6>By: <.username user={@build.builder} /></h6>
+          <div class="flex flex-row justify-between">
+            <h5 class="truncate mb-2 text-4xl font-medium leading-tight text-neutral-800">
+              <.link navigate={~p"/builds/#{@build.slug}"}>
+                <%= @build.name %>
+              </.link>
+            </h5>
+            <h6>By: <.username user={@build.builder} /></h6>
 
-          <.link patch={~p"/builds?make=#{@build.manufacturer.slug}"}>
-            <%= @build.manufacturer.name %>
-          </.link>
-          <.link patch={~p"/builds?model=#{@build.model.slug}"}>
-            <%= @build.model.name %>
-          </.link>
+            <.link patch={~p"/builds?make=#{@build.manufacturer.slug}"}>
+              <%= @build.manufacturer.name %>
+            </.link>
+            <.link patch={~p"/builds?model=#{@build.model.slug}"}>
+              <%= @build.model.name %>
+            </.link>
+          </div>
+
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-10">
+            <div class="flex flex-row justify-between">
+              <div class="font-black">Engine:</div>
+              <div :if={@build.engine}>
+                <%= @build.engine.manufacturer.name %> <%= @build.engine.name %>
+              </div>
+            </div>
+            <div class="flex flex-row justify-between">
+              <div class="font-black">Carburetor:</div>
+              <div :if={@build.carb_tuning && @build.carb_tuning.carburetor}>
+                <%= @build.carb_tuning.carburetor.manufacturer.name %> <%= @build.carb_tuning.carburetor.name %>
+              </div>
+            </div>
+            <div class="flex flex-row justify-between">
+              <div class="font-black">Exhaust:</div>
+              <div :if={@build.exhaust}>
+                <%= @build.exhaust.manufacturer.name %> <%= @build.exhaust.name %>
+              </div>
+            </div>
+            <div class="flex flex-row justify-between">
+              <div class="font-black">Exhaust:</div>
+              <div :if={@build.exhaust}>
+                <%= @build.exhaust.manufacturer.name %> <%= @build.exhaust.name %>
+              </div>
+            </div>
+            <div class="flex flex-row justify-between">
+              <div class="font-black">Clutch:</div>
+              <div :if={@build.clutch}>
+                <%= @build.clutch.manufacturer.name %> <%= @build.clutch.name %>
+              </div>
+            </div>
+            <div class="flex flex-row justify-between">
+              <div class="font-black">Ignition:</div>
+              <div :if={@build.ignition}>
+                <%= @build.ignition.manufacturer.name %> <%= @build.ignition.name %>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
